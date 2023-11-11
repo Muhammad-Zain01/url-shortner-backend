ca58e45746f7e2f9ebcca2e8b96d28648a102df9
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-11 17:16:22 +0500

    Writing Boiler Plate Code for Starting Up

---
+++ controller/url.controller.js
@@ -0,0 +1,24 @@
+function getURL(req, res) {
+    const keyword = req.params.keyword
+    // WORKING
+}
+
+function saveURL(req, res) {
+    const body = req.body
+    // WORKING
+}
+function updateURL(req, res) {
+    const body = req.body
+    // WORKING
+}
+function removeURL(req, res) {
+    const body = req.body
+    // WORKING
+}
+
+module.exports = {
+    getURL,
+    saveURL,
+    updateURL,
+    removeURL
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const UrlController = require('./controller/url.controller')
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+app.get('/:keyword', UrlController.getURL)
+app.post('/events/save', UrlController.saveURL)
+app.put('/events/update', UrlController.updateURL)
+app.delete('/events/remove', UrlController.removeURL)
+
+app.listen(PORT, () => {
+    console.log(`Listening at Port ${PORT}`)
+})
