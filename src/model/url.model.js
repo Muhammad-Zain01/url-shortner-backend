59727fee4a793c49f53a3cece58b931c946727ec
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-12 16:25:28 +0500

    Update url.model.js

---
+++ src/model/url.model.js
@@ -0,0 +1,18 @@
+let URL_DATABASE = [
+    {
+        id: 0,
+        keyword: 'BxkeYl',
+        url: 'www.google.com'
+    }
+]
+let ID = 1;
+const getUrlData = (keyword) => {
+    const result = URL_DATABASE.filter(item => item.keyword == keyword);
+    if (result.length) {
+        return { url: result.url }
+    }
+    return false;
+}
+
+
+module.exports = { getUrlData }
