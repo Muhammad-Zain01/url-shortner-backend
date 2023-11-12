6ff54189a2e35b40af62e541296a83b61bcec05b
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-12 16:58:46 +0500

    Create helper.js

---
+++ src/utils/helper.js
@@ -0,0 +1,13 @@
+function makeKeyword(length = 8) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+module.exports = { makeKeyword }
