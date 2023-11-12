bf28bcf149dd265f21c083498384c0a4a69abdd1
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-12 16:58:51 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const http = require('http');
 const app = require('./app')
-
 const PORT = process.env.PORT || 8000;
-
 const server = http.createServer(app);
 server.listen(PORT, () => {
     console.log(`Listening at PORT ${PORT}`)
