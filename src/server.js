76cd37c84b05fb7905bc4409b9bfc201baaeb758
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-15 12:20:08 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 const http = require('http');
 const app = require('./app');
-const Database = require('./utils/database')
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
-
 
 server.listen(PORT, () => {
     console.log(`Listening at PORT ${PORT}`)
