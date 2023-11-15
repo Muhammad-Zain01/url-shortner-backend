ae47de149cafefb4582dcffa5df41724406a0f8a
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-15 11:18:07 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 const http = require('http');
 const app = require('./app');
 const Database = require('./utils/database')
+
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-const check = async () => {
-    const dbInstance = new Database()
-    console.log(await dbInstance.connect())
-    console.log(await dbInstance.getDocs())
-}
-check()
-// server.listen(PORT, () => {
-//     console.log(`Listening at PORT ${PORT}`)
-// })
+
+server.listen(PORT, () => {
+    console.log(`Listening at PORT ${PORT}`)
+})
 
