a1cc042225af2bbeec49d814f0a644b512f8399a
zmemon <zainmemon010@gmail.com>
2023-11-16 16:11:53 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const UrlController = require("./controller/url.controller")
 const UserController = require('./controller/user.controller')
+const { AuthenticateUser } = require('./middleware/authenticate')
 app.use(express.json())
 app.use(cors({ origin: 'http://localhost:5173' }))
 
@@ -10,9 +11,12 @@ app.use(cors({ origin: 'http://localhost:5173' }))
 // app.get('/', UrlController.getAllURL)
 // app.get('/:keyword', UrlController.getURL)
 app.post('/events/verify/:username', UserController.CheckUsername)
+
 app.post('/events/register', UserController.RegisterUser)
 app.post('/events/login', UserController.LoginUser)
-app.post('/events/save', UrlController.saveURL)
+app.post('/events/add', AuthenticateUser, UrlController.addURL)
+app.post('/events/get-urls', AuthenticateUser, UrlController.getData)
+
 app.put('/events/update/:keyword', UrlController.updateURL)
 app.delete('/events/remove/:keyword', UrlController.removeURL)
 
