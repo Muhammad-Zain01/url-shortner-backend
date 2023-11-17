447693fd6e666ea1f69d7914c55c18bc0252de28
zmemon <zainmemon010@gmail.com>
2023-11-17 11:03:56 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,19 +3,25 @@ const app = express();
 const cors = require('cors');
 const UrlController = require("./controller/url.controller")
 const UserController = require('./controller/user.controller')
-const { AuthenticateUser } = require('./middleware/authenticate')
+const AuthenticateUser = require('./middleware/authenticate')
+const ProxyMiddleware = require('./middleware/proxy');
+
 app.use(express.json())
 app.use(cors({ origin: 'http://localhost:5173' }))
 
-
-// app.get('/', UrlController.getAllURL)
-// app.get('/:keyword', UrlController.getURL)
+app.get('/proxy/:url', ProxyMiddleware)
 app.post('/events/verify/:username', UserController.CheckUsername)
-
 app.post('/events/register', UserController.RegisterUser)
 app.post('/events/login', UserController.LoginUser)
 app.post('/events/add', AuthenticateUser, UrlController.addURL)
 app.post('/events/get-urls', AuthenticateUser, UrlController.getData)
+
+
+// app.get('/', UrlController.getAllURL)
+// app.get('/:keyword', UrlController.getURL)
+
+
+
 
 app.put('/events/update/:keyword', UrlController.updateURL)
 app.delete('/events/remove/:keyword', UrlController.removeURL)
