89c0b371c342594ab0da67fc23bcab65d20cf4be
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-18 20:22:46 +0500

    Update url.router.js

--- src/router/url.router.js
+++ src/router/url.router.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const UrlRouter = express.Router();
 
-
 UrlRouter.post('/events/add', AuthenticateUser, UrlController.addURL)
 UrlRouter.post('/events/get-urls', AuthenticateUser, UrlController.getData)
 UrlRouter.post('/capture', UrlController.CaptureUser)
 
+module.export = UrlRouter;
