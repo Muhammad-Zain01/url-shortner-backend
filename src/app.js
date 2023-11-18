b66aab6308b11d3e7f4e75502d97bf3ed66fa904
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-18 20:33:17 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,26 +8,25 @@ const UserRouter = require('./router/user.router')
 const AuthenticateUser = require('./middleware/authenticate')
 const ProxyMiddleware = require('./middleware/proxy');
 
-app.use(cors({ origin: 'http://127.0.0.1:5173' }))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 app.get('/proxy/:url', ProxyMiddleware)
-
-app.post('/events/verify/:username', UserController.CheckUsername)
-app.post('/events/verify/keyword/:keyword', UserController.VerifyKeyword)
-app.post('/events/register', UserController.RegisterUser)
-app.post('/events/login', UserController.LoginUser)
-app.post('/events/add', AuthenticateUser, UrlController.addURL)
-app.post('/events/get-urls', AuthenticateUser, UrlController.getData)
-app.post('/capture', UrlController.CaptureUser)
+app.use(UrlRouter)
+app.use(UserRouter)
+// app.post('/events/verify/:username', UserController.CheckUsername)
+// app.post('/events/verify/keyword/:keyword', UserController.VerifyKeyword)
+// app.post('/events/register', UserController.RegisterUser)
+// app.post('/events/login', UserController.LoginUser)
+// app.post('/events/add', AuthenticateUser, UrlController.addURL)
+// app.post('/events/get-urls', AuthenticateUser, UrlController.getData)
+// app.post('/capture', UrlController.CaptureUser)
 
 // app.get('/', UrlController.getAllURL)
 // app.get('/:keyword', UrlController.getURL)
 
 
-
-
-app.put('/events/update/:keyword', UrlController.updateURL)
-app.delete('/events/remove/:keyword', UrlController.removeURL)
+// app.put('/events/update/:keyword', UrlController.updateURL)
+// app.delete('/events/remove/:keyword', UrlController.removeURL)
 
 module.exports = app;
