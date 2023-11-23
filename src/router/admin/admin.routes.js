059ae3cdf239939bd03330b6940ad4b6286d947a
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-23 11:08:59 +0500

    Update admin.routes.js

--- src/router/admin/admin.routes.js
+++ src/router/admin/admin.routes.js
@@ -9,5 +9,7 @@ AdminRoutes.post('/events/remove-url/:keyword', AdminController.removeURL)
 AdminRoutes.post('/events/get-urls', AdminController.adminGetData)
 AdminRoutes.post('/events/keyword/:keyword', AdminController.adminVerifyKeyword)
 AdminRoutes.post('/events/dasboard-data', AdminController.adminDashboardData)
+AdminRoutes.post('/events/get-display-name', AdminController.DisplayName)
+AdminRoutes.post('/events/set-display-name', AdminController.setDisplayName)
 
 module.exports = AdminRoutes;
