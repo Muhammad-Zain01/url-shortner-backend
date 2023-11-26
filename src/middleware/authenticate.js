f303f102fd279637dc70fdcbe3c230f697dba824
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-26 10:18:33 +0500

    Update authenticate.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,28 +1,19 @@
-const DBInstance = require('../utils/database');
+const { users } = require('../db/schema')
 const AuthenticateUser = async (req, res, next) => {
     const body = req.body;
     const user = body?.user;
-    const PARAM = []
-    if (user) {
-        const token = user?.token;
-        const username = user?.username;
-        const doc = await DBInstance.getData('users')
-        try {
-            const data = await doc.where({ username })
-            if (data.length > 0) {
-                const user_id = Buffer.from(data[0]._id.toHexString(), 'hex').toString('base64');
-                PARAM.push({ cond: user_id == token })
-                if (user_id == token) {
-                    return next();
-                } else {
-                    return res.json({ status: 0, message: 'INVALID_TOKEN', param: PARAM })
-                }
-            } else {
-                return res.json({ status: 0, message: 'NON_USER', param: PARAM })
-            }
-        } catch (e) {
-            return res.json({ error: e, param: PARAM })
+    const username = user?.username;
+    const token = user?.token;
+    const result = await users.findOne({ username })
+    if (result) {
+        const user_id = Buffer.from(result._id.toHexString(), 'hex').toString('base64');
+        if (user_id == token) {
+            return next();
+        } else {
+            return res.json({ status: 0, message: 'INVALID_TOKEN' })
         }
+    }else{
+        return res.json({status: 0, message: 'INVALID_USER'})
     }
 }
 
