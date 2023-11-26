be3577c052f7b67ebb39305b43fdfae0909406c1
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-26 08:52:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./src/app');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening at PORT ${PORT}`)
-})
+mongoose.connection.once('open', () => console.log('MONGO DB Connected'))
+mongoose.connection.on('error', (err) => console.error(err))
+
+async function startServer() {
+    await mongoose.connect(process.env.MONGO_DB_URL);
+    server.listen(PORT, () => {
+        console.log(`Listening at PORT ${PORT}`)
+    })
+}
+
+startServer() // Starting a Server
 
