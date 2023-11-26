b8bddfb82c89bb7b78273dc24cbe209faa443254
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-26 08:54:01 +0500

    Delete global.routes.js

--- src/router/global/global.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const GlobalController = require('./global.controller')
-const GlobalRouter = express.Router();
-
-GlobalRouter.get('/proxy/:url', GlobalController.globalProxyMiddleware)
-GlobalRouter.get('/:keyword', GlobalController.globalCaptureUser)
-module.exports = GlobalRouter;
