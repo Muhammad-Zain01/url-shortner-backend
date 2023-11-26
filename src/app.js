f4491c333b33d30c82c23063bf1ff02481db6d1f
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-26 08:53:03 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const AdminRoutes = require('./router/admin/admin.routes')
-const AuthRouter = require('./router/auth/auth.routes')
-const GlobalRoutes = require('./router/global/global.routes')
+
+// const AdminRoutes = require('./router/admin/admin.routes')
+const AuthRouter = require('./router/auth.routes')
+const GlobalRoutes = require('./router/global.routes')
+
 const app = express();
+
 app.use(cors({ origin: '*' }))
 app.use(express.json())
+
 app.use("/auth", AuthRouter)
 app.use(GlobalRoutes)
-app.use("/admin", AdminRoutes)
-app.use('/', (req, res) => {
-    res.send("Url Shortner Working Fine");
-})
+// app.use("/admin", AdminRoutes)
+
 module.exports = app;
