15b168ffef08530656940979a2b9a7815fadeff6
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-11-26 08:53:06 +0500

    Create admin.controller.js

---
+++ src/controller/admin.controller.js
@@ -0,0 +1,90 @@
+const { makeKeyword } = require('../utils/helper');
+
+const {
+    getUrls,
+    insertUrl,
+    getDisplayName,
+    verifyKeyword,
+    deleteUrl,
+    getUserDashboardData,
+    updateDisplayName,
+    getUser,
+    updatePassword
+} = require('../../model/admin.modal')
+
+async function httpGetData(req, res) {
+    const body = req.body;
+    const user = body?.user;
+    const result = await getUrls(user);
+    res.json(result)
+}
+async function httpDashboardData(req, res) {
+    const body = req.body;
+    const user = body?.user;
+    const result = await getUserDashboardData(user);
+    res.json(result)
+}
+async function httpRemoveUrl(req, res) {
+    const keyword = req.params.keyword
+    const result = await deleteUrl(keyword);
+    res.json(result)
+}
+async function httpAddUrl(req, res) {
+    const body = req.body
+    const username = body?.user?.username
+    let keyword = body.keyword;
+    if (keyword == "") { keyword = makeKeyword() }
+
+    const links = {
+        user: username,
+        url: body?.url,
+        title: body?.title,
+        keyword,
+        icon: body?.icon
+    }
+    const result = await insertUrl(links);
+    res.json(result);
+}
+async function httpVerifyKeyword(req, res) {
+    const keyword = req.params.keyword
+    const result = await verifyKeyword(keyword);
+    res.json(result);
+}
+async function httpDisplayName(req, res) {
+    const body = req.body;
+    const user = body?.user;
+    const result = await getDisplayName(user);
+    res.json(result);
+}
+async function httpUpdateDisplayName(req, res) {
+    const body = req.body;
+    const user = body?.user;
+    const name = body?.name;
+    const result = await updateDisplayName(user, name);
+    res.json(result)
+}
+async function httpGetUserData(req, res) {
+    const body = req.body;
+    const user = body?.user;
+    const result = await getUser(user);
+    res.json(result);
+}
+async function httpUpdatePassword(req, res) {
+    const body = req.body;
+    const user = body?.user;
+    const currentPassword = body?.currentPassword;
+    const newPassword = body?.newPassword;
+    const result = await updatePassword(user, currentPassword, newPassword);
+    res.json(result);
+}
+module.exports = {
+    httpGetData,
+    httpDashboardData,
+    httpRemoveUrl,
+    httpAddUrl,
+    httpVerifyKeyword,
+    httpDisplayName,
+    httpUpdateDisplayName,
+    httpGetUserData,
+    httpUpdatePassword,
+}
