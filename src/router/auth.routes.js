7a69a59305c486be91c3271bbe4dfbc96dec19b8
zmemon <zainmemon010@gmail.com>
2023-11-30 13:12:02 +0500

    Update auth.routes.js

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -5,5 +5,7 @@ const AuthRouter = express.Router();
 AuthRouter.post('/verify/:username', AuthController.httpVerifyUsername)
 AuthRouter.post('/register', AuthController.httpRegisterUser)
 AuthRouter.post('/login', AuthController.httpLoginUser)
+AuthRouter.post('/forgot-password/email-verfiy', AuthController.httpForgotPasswordEmailVerification)
+AuthRouter.post('/forgot-password/reset-code', AuthController.httpResetCode)
 
 module.exports = AuthRouter;
