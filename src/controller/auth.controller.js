8d28bcce359fb97c53ad8da957c3708982a071d2
Muhammad Zain <83939353+Muhammad-Zain01@users.noreply.github.com>
2025-04-10 12:03:47 +0500

    Updates

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,38 +1,46 @@
-const { isUserNameAvailable, registerUser, authenticateUser, ForgotPasswordEmailVerfication, ResetCode } = require('../model/auth.model');
+const {
+  isUserNameAvailable,
+  registerUser,
+  authenticateUser,
+  ForgotPasswordEmailVerfication,
+  ResetCode,
+} = require("../model/auth.model");
 async function httpVerifyUsername(req, res) {
-    const username = req.params.username;
-    res.json(await isUserNameAvailable(username))
+  const username = req.params.username;
+  res.json(await isUserNameAvailable(username));
 }
 async function httpRegisterUser(req, res) {
-    const body = req.body;
-    const username = body?.username
-    const email = body?.email
-    const password = req.body?.password
-    res.json(await registerUser(username, email, password))
+  const body = req.body;
+  const username = body?.username;
+  const email = body?.email;
+  const password = req.body?.password;
+  res.json(await registerUser(username, email, password));
 }
 async function httpLoginUser(req, res) {
-    const body = req.body;
-    const username = body?.username
-    const password = body?.password
-    res.json(await authenticateUser(username, password));
+  const body = req.body;
+  const username = body?.username;
+  const password = body?.password;
+  const result = await authenticateUser(username, password);
+  console.log("result", result);
+  res.json(result);
 }
 async function httpForgotPasswordEmailVerification(req, res) {
-    const body = req.body;
-    const username = body?.username;
-    const result = await ForgotPasswordEmailVerfication(username);
-    res.json(result);
+  const body = req.body;
+  const username = body?.username;
+  const result = await ForgotPasswordEmailVerfication(username);
+  res.json(result);
 }
 async function httpResetCode(req, res) {
-    const body = req.body;
-    const token = body?.token;
-    const code = body?.code;
-    const result = await ResetCode(token, code);
-    res.json(result);
+  const body = req.body;
+  const token = body?.token;
+  const code = body?.code;
+  const result = await ResetCode(token, code);
+  res.json(result);
 }
 module.exports = {
-    httpResetCode,
-    httpForgotPasswordEmailVerification,
-    httpVerifyUsername,
-    httpRegisterUser,
-    httpLoginUser,
-}
+  httpResetCode,
+  httpForgotPasswordEmailVerification,
+  httpVerifyUsername,
+  httpRegisterUser,
+  httpLoginUser,
+};
--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -1,116 +1,132 @@
-const { EncryptPassword, ComparePassword, errorResponse, generateVerificationCode } = require('../utils/helper')
-const { users } = require('../db/schema')
-const Email = require('../utils/emailSender');
-const { Encode, Decode } = require('../utils/jwt')
-async function isUserNameAvailable(username) {
-    try {
-        const result = await users.findOne({ username })
-        if (result) return { status: 0 }
-        else return { status: 1 }
-    } catch (error) {
-        return errorResponse(error)
-    }
-}
-async function registerUser(username, email, pass) {
-    try {
-        const displayName = username
-        const password = await EncryptPassword(pass)
-        const isVerified = false;
-        const verificationCode = generateVerificationCode();
-        const user = {
-            username,
-            email,
-            password,
-            displayName,
-            isVerified,
-            verificationCode
-        }
-        await users.create(user)
-        await Email.SendVerificationEmail(email, displayName, verificationCode)
-        return { status: 1 }
-    } catch (error) {
-        return errorResponse(error)
-    }
-}
-async function authenticateUser(username, password) {
-    try {
-        const result = await users.findOne({ username })
-        if (result) {
-            const isCorrectPassword = await ComparePassword(password, result.password)
-            if (isCorrectPassword) {
-                const data = {
-                    username: result.username,
-                    email: result.email,
-                };
-                const token = Encode(data);
-                return {
-                    status: 1,
-                    data: { token }
-                }
-            } else {
-                return {
-                    status: 0,
-                    message: 'INVALID_PASSWORD'
-                }
-            }
-        } else {
-            return { status: 0, message: "INVALID_USER" };
+const {
+  EncryptPassword,
+  ComparePassword,
+  errorResponse,
+  generateVerificationCode,
+} = require("../utils/helper");
+const { users } = require("../db/schema");
+const Email = require("../utils/emailSender");
+const { Encode, Decode } = require("../utils/jwt");
 
-        }
-    }
-    catch (error) {
-        return errorResponse(error)
-    }
+async function isUserNameAvailable(username) {
+  try {
+    const result = await users.findOne({ username });
+    if (result) return { status: 0 };
+    else return { status: 1 };
+  } catch (error) {
+    return errorResponse(error);
+  }
 }
+
+async function registerUser(username, email, pass) {
+  try {
+    const displayName = username;
+    const password = await EncryptPassword(pass);
+    const isVerified = false;
+    const verificationCode = generateVerificationCode();
+    const user = {
+      username,
+      email,
+      password,
+      displayName,
+      isVerified,
+      verificationCode,
+    };
+    await users.create(user);
+    console.log("auth", user);
+    // await Email.SendVerificationEmail(email, displayName, verificationCode);
+    return { status: 1 };
+  } catch (error) {
+    return errorResponse(error);
+  }
+}
+
+async function authenticateUser(username, password) {
+  try {
+    const result = await users.findOne({ username });
+    if (result) {
+      const isCorrectPassword = await ComparePassword(
+        password,
+        result.password
+      );
+      if (isCorrectPassword) {
+        const data = {
+          username: result.username,
+          email: result.email,
+        };
+        const token = Encode(data);
+        return {
+          status: 1,
+          token,
+          user: {
+            username: result.username,
+            email: result.email,
+          },
+        };
+      } else {
+        return {
+          status: 0,
+          message: "INVALID_PASSWORD",
+        };
+      }
+    } else {
+      return { status: 0, message: "INVALID_USER" };
+    }
+  } catch (error) {
+    return errorResponse(error);
+  }
+}
+
 async function ForgotPasswordEmailVerfication(username) {
-    try {
-        const result = await users.findOne({ username })
-        if (result) {
-            const email = result.email;
-            const displayName = result.displayName;
-            const verificationCode = generateVerificationCode();
-            await users.updateOne({ username }, { verificationCode })
-            await Email.SendVerificationEmail(email, displayName, verificationCode)
-            const token = Encode({ username })
-            return { status: 1, token }
-        } else {
-            return { status: 0, message: 'INVALID_USER' }
-        }
-    } catch (error) {
-        return errorResponse(error)
+  try {
+    const result = await users.findOne({ username });
+    if (result) {
+      const email = result.email;
+      const displayName = result.displayName;
+      const verificationCode = generateVerificationCode();
+      await users.updateOne({ username }, { verificationCode });
+      await Email.SendVerificationEmail(email, displayName, verificationCode);
+      const token = Encode({ username });
+      return { status: 1, token };
+    } else {
+      return { status: 0, message: "INVALID_USER" };
     }
+  } catch (error) {
+    return errorResponse(error);
+  }
 }
+
 async function ResetCode(token, code) {
-    try {
-        const data = Decode(token)
-        if (data) {
-            const { username } = data
-            const result = await users.findOne({ username })
-            if (result) {
-                if (result.verificationCode == code) {
-                    const newPassword = generateVerificationCode();
-                    const email = result.email;
-                    const displayName = result.displayName;
-                    const newHashedPassword = await EncryptPassword(newPassword)
-                    await users.updateOne({ username }, { password: newHashedPassword })
-                    await Email.PasswordResetEmail(email, displayName, newPassword)
-                    return { status: 1 }
-                } else {
-                    return { status: 0, message: 'INVALID_CODE' }
-                }
-            }
+  try {
+    const data = Decode(token);
+    if (data) {
+      const { username } = data;
+      const result = await users.findOne({ username });
+      if (result) {
+        if (result.verificationCode == code) {
+          const newPassword = generateVerificationCode();
+          const email = result.email;
+          const displayName = result.displayName;
+          const newHashedPassword = await EncryptPassword(newPassword);
+          await users.updateOne({ username }, { password: newHashedPassword });
+          await Email.PasswordResetEmail(email, displayName, newPassword);
+          return { status: 1 };
         } else {
-            return { status: 0, message: 'INVALID_TOKEN' }
+          return { status: 0, message: "INVALID_CODE" };
         }
-    } catch (error) {
-        return errorResponse(error)
+      }
+    } else {
+      return { status: 0, message: "INVALID_TOKEN" };
     }
+  } catch (error) {
+    return errorResponse(error);
+  }
 }
 
 module.exports = {
-    ResetCode,
-    ForgotPasswordEmailVerfication,
-    isUserNameAvailable,
-    registerUser,
-    authenticateUser
-}
+  ResetCode,
+  ForgotPasswordEmailVerfication,
+  isUserNameAvailable,
+  registerUser,
+  authenticateUser,
+};
